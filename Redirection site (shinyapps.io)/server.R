d74b431355e93d29cebc45dbe9cd8c269d3f66ef
Maya Mathur <mmathur@stanford.edu>
2020-12-25 10:32:06 -0800

    make redirection site from shinyapps to custom domain

---
+++ Redirection site (shinyapps.io)/server.R
@@ -0,0 +1,7 @@
+library(shiny)
+
+# Define server logic required to generate and plot a random distribution
+shinyServer(function(input, output) {
+
+  
+})
--- Redirection site (shinyapps.io)/ui.R
+++ Redirection site (shinyapps.io)/ui.R
@@ -1,1 +1,13 @@
- This is a placeholder file because git doesn't track empty repos. 
+
+library(shiny)
+
+# Define UI for application that plots random distributions 
+shinyUI(fluidPage(
+    
+    hr(),
+    
+    # Application title
+    titlePanel( HTML( "The E-value calculator has moved to <a href='https://www.evalue-calculator.com'>www.evalue-calculator.com</a>. Enjoy!") )
+    
+    
+))
