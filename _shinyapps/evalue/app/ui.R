9863fff797111c92662603e82190a600c87e72c3
Maya Mathur <mmathur@stanford.edu>
2021-02-26 15:51:26 -0800

    deploy evalue; simplify UI

--- _shinyapps/evalue/app/ui.R
+++ _shinyapps/evalue/app/ui.R
@@ -7,90 +7,19 @@ nonnull.mess = 'Note: You are calculating a "non-null" E-value, i.e., an E-value
 
 # message to display for OLS
 OLS.mess = 'Note: Using the standard deviation of the outcome yields a conservative approximation
-of the standardized mean difference. For a non-conservative estimate, you could instead use the estimated residual standard deviation from your linear
+of the standardized mean difference. For a non-conservative estimate, you could instead use the estimated ressidual standard deviation from your linear
 regression model. Regardless, the reported E-value for the confidence interval treats the
 standard deviation as known, not estimated.'
 
 
 
-ui = navbarPage( "E-value calculator", id = "navbar",
+ui =
 
+    fluidPage(
 
             theme = shinytheme("flatly"),
 
-            tabPanel( "Instructions",
-
-
-                      mainPanel(
-
-
-                          # Google Analytics
-                          # this JS is from the "tracking code" available on the Google Analytics website
-                          tags$head( HTML( '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125815848-1"></script>
-                        <script>
-                                                       window.dataLayer = window.dataLayer || [];
-                                                       function gtag(){dataLayer.push(arguments);}
-                                                       gtag("js", new Date());
-
-                                                       gtag("config", "UA-125815848-1");
-                                                       </script>' ) ),
-                          # END Google Analytics
-
-                          wellPanel(  HTML(paste("<b>Computing an E-value</b>",
-
-                                                 'The tab "Compute an E-value" computes the E-value, defined as the minimum strength of association
-                                      on the risk ratio scale that an unmeasured confounder would need to have with both the exposure
-                                      and the outcome, conditional on the measured covariates, to fully explain away a specific
-                                      exposure-outcome association. Note that for outcome types other than relative risks, assumptions
-                                      are involved with the approximate conversions used. See citation (2) for details.',
-
-                                                 'Alternatively, you can consider the confounding strength capable of moving the observed
-                                      association to any other value (e.g. attenuating the observed association to a true causal
-                                      effect that is no longer scientifically important, or alternatively increasing a near-null
-                                      observed association to a value that is of scientific importance). For this purpose, simply
-                                      type a non-null effect size into the box "True causal effect to which to shift estimate"
-                                      when computing the E-value.',
-
-                                                 "<b>Computing a bias factor</b>",
-
-                                                 "Additionally, if you have substantive knowledge on the strength of the relationships
-                                      between the unmeasured confounder(s) and the exposure and outcome, you can use these
-                                      numbers to <a href='https://bias-factor.hmdc.harvard.edu'>calculate the bias factor</a>.",
-                                                 sep="<br/><br/>"))
-
-
-                          ),
-
-                          width=6
-
-
-                      ),
-
-                      sidebarPanel(
-                          HTML(paste("<b>Please use the following citations:</b>",
-
-                                     "<a href='https://journals.lww.com/epidem/Citation/publishahead/Website_and_R_Package_for_Computing_E_Values.98679.aspx'>(1) Mathur MB, Ding P, Riddell CA, VanderWeele TJ. (2018). Website and R package
-                                               for computing E-values. <i>Epidemiology</i>, 29(5), e45-e47.",
-
-                                     "<a href='http://annals.org/aim/article-abstract/2643434/sensitivity-analysis-observational-research-introducing-e-value?doi=10.7326%2fM16-2607'>(2) VanderWeele TJ,
-                                               & Ding P. (2017). Sensitivity analysis in observational research: introducing the
-                                               E-value. <i>Annals of Internal Medicine</i>, 167(4), 268-274.</a>",
-
-
-                                     "<b>Bug reports</b>",
-
-                                     "Submit any bug reports to: <i>mmathur [AT] stanford [DOT] edu</i> or open
-                                                    an issue on <a href='https://github.com/mayamathur/evalue/issues'>Github</a>.",
-
-                                     sep="<br/><br/>") )
-
-                      )
-            ),
-
-            #)
-
-
-            tabPanel( "Compute an E-value",
+            mainPanel(
 
                       mainPanel(
                           selectInput( "outcomeType", label = "Outcome type",
@@ -214,14 +143,15 @@ ui = navbarPage( "E-value calculator", id = "navbar",
                           conditionalPanel( condition = "input.makeplot == true",
                                             HTML(paste("<br>Each point along the curve defines a joint relationship between the two sensitivity parameters that could potentially explain away the estimated effect.",
                                                        " If one of the two parameters is smaller than the E-value, the other must be larger, as defined by the plotted curve."))
-                          ),
+                          )
+                          ,
 
                           width = 6
 
                       ) # end contour plot panel
-            )
+            )  # end mainPanel
 
-)
+)  # end fluidPage
 
 
 # tags$footer(title="Your footer here", align = "right", style = "
