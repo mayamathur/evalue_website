001afad6ce6db2ab6da34401a6e3ef75ab5cb3d3
Maya Mathur <mmathur@stanford.edu>
2021-03-01 11:37:50 -0800

    try to widen text col

--- _shinyapps/meta/app/global.R
+++ _shinyapps/meta/app/global.R
@@ -8,12 +8,15 @@ library(shinydashboard)
 library(shinyWidgets)
 library(ggplot2)
 
+# if sourcing package from CRAN
 library(EValue) #include confounded_meta and sens_plot below to test, will eventually be loaded into EValue package and can remove the functions below
 
-# utils.R
-# EValue.R
-# meta-analysis.R
-# effect_measures.R
+# # if using local development versions
+# setwd("~/Dropbox/Personal computer/Independent studies/R packages/EValue package (git)/evalue_package/EValue/R")
+# source("utils.R")
+# source("EValue.R")
+# source("meta-analysis.R")
+# source("effect_measures.R")
 
 
 library(MetaUtility)
@@ -25,16 +28,16 @@ library(boot)
 # keeps original error messages
 options(shiny.sanitize.errors = FALSE)
 
-### CHANGE THIS ###
+
 # message to display if non-null true value
 nonnull.mess = 'Note: You are calculating a "non-null" E-value, i.e., an E-value for the minimum
 amount of unmeasured confounding needed to move the estimate and confidence interval
 to your specified true value rather than to the null value.'
 
-### CHANGE THIS ###
+
 # message to display for OLS
 OLS.mess = 'Note: Using the standard deviation of the outcome yields a conservative approximation
 of the standardized mean difference. For a non-conservative estimate, you could instead use the estimated residual standard deviation from your linear
-regression model. Regardless, the reported E-value for the confidence interval treats the 
+regression model. Regardless, the reported E-value for the confidence interval treats the
 standard deviation as known, not estimated.'
 
